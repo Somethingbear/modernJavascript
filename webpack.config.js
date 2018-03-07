c043c199a3594bbbff07e97622265a785123f9cd
somethingbear <chenliqian1995@gmail.com>
2018-03-07 18:12:35 +0800

    vendor bundling,packing css,babel

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,32 @@ module.exports = {
         path: path.resolve(__dirname, './dist'),
         filename: '[name].bundle.js'
     },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                /*processed in reverse array order*/
+                use: ['style-loader', 'css-loader']
+            },
+
+            {
+                test: /\.js$/,
+                exclude: [/node_mudules/],
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['env', {
+                                    modules: false
+                                    //turn off module transpilling to allow tree shaking in webpack
+                                    //webpack2+已经支持 import export，不需要babel来转换，babel会转化成require，是synchronize，不适合client side使用
+                                }]
+                        }
+                    }
+                ]
+            }
+        ]
+    },
     plugins: [
         new webpack.optimize.CommonsChunkPlugin({
             name: 'vendor',
