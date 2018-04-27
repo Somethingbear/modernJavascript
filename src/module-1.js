85fa70c2cd0e4e4dea84dcde973a682c72cd66ff
somethingbear <chenliqian1995@gmail.com>
2018-04-27 16:20:08 +0800

    end

--- src/module-1.js
+++ src/module-1.js
@@ -4,7 +4,8 @@ import './module-1.css';
 
 export default function() {
     var el = document.querySelector('#module-1');
-    el.textContent = _.join(['Module', '1'], ' ');
+    let module = Symbol('module 1');
+    el.textContent = `Hello ${module.toString()}`;
 }
 
 export var n = 101;
--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,7 @@ var webpack = require('webpack');
 module.exports = {
     context: path.resolve(__dirname, './src'),
     entry: {
-        main: './main.js',
-        vendor: ['lodash']
+        main: ['babel-polyfill', './main.js', ],
     },
     output: {
         path: path.resolve(__dirname, './dist'),
@@ -14,26 +13,25 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.css$/,
-                /*processed in reverse array order*/
-                use: ['style-loader', 'css-loader']
-            },
-
-            {
                 test: /\.js$/,
                 exclude: [/node_mudules/],
                 use: [
                     {
                         loader: 'babel-loader',
                         options: {
-                            presets: ['env', {
-                                    modules: false
-                                    //turn off module transpilling to allow tree shaking in webpack
-                                    //webpack2+已经支持 import export，不需要babel来转换，babel会转化成require，是synchronize，不适合client side使用
-                                }]
+                            presets: [
+                                ['env', {modules: false}]
+                                //turn off module transpilling to allow tree shaking in webpack
+                                //webpack2+已经支持 import export，不需要babel来转换，babel会转化成require，是synchronize，不适合client side使用
+                            ]
                         }
                     }
                 ]
+            },
+            {
+                test: /\.css$/,
+                /*processed in reverse array order*/
+                use: ['style-loader', 'css-loader']
             }
         ]
     },
@@ -41,7 +39,7 @@ module.exports = {
         new webpack.optimize.CommonsChunkPlugin({
             name: 'vendor',
             filename: 'vendor.bundle.js',
-            minChunks: Infinity
+            minChunks: 2
         }),
     ],
 };
